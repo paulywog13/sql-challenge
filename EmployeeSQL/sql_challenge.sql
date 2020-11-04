4e05247675a881b485af6cffd1cbd1af38b66547
Pauly Richmeier <paul_richmeier@yahoo.com>
2020-11-03 23:15:17 -0700

    DataBase created, tables created, and files uploaded

---
+++ EmployeeSQL/sql_challenge.sql
@@ -0,0 +1,11 @@
+CREATE TABLE departments(
+	dept_no VARCHAR,
+	dept_name VARCHAR);
+
+SELECT * FROM departments;
+
+CREATE TABLE dept_emp(
+	emp_no INT,
+	dept_no VARCHAR);
+
+SELECT * FROM dept_emp;
